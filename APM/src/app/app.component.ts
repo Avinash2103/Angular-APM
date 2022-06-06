16e84134f5a61ec0e4c4e29379c5bb87e6153e90
AvinashSutariya <avinash.s@simformsolutions.com>
2022-06-06 11:57:26 +0530

    Added product-list component

--- APM/src/app/app.component.ts
+++ APM/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+//import {ProductListComponent} from './products/product-list/product-list.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   pageTitle = 'Acme Product Management';
+  
 }
